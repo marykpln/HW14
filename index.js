fb0e26cd8ab6ee5ebfefcd238789167ba3892266
MariiaKpln <kpln.mary@gmail.com>
2025-02-15 18:49:09 +0200

    HW14

---
+++ index.js
@@ -0,0 +1,77 @@
+function some(array, fun) {
+  let result = false;
+  for (let i = 0; i < array.length; i++) {
+    if (fun(array[i], i)) {
+      result = true;
+    }
+  }
+  return result;
+}
+//TODO
+//returns true if at least one element of the array match a condition given in the function (fun)
+
+function evenNumber(num) {
+  return num % 2 == 0;
+}
+
+function every(array, fun) {
+  let result = true;
+  for (let i = 0; i < array.length; i++) {
+    if (fun(array[i], i) == false) {
+      result = false;
+    }
+  }
+  return result;
+  //TODO
+  //returns true if all elemnts of the array match a condition given in the function (fun)
+}
+
+function elmGreaterIndex(elem, index) {
+  return elem > index;
+}
+let array = ["abc", 23];
+console.log(
+  `using "some" function for even numbers array: ${array}, function ${evenNumber}, result: ${some(
+    array,
+    evenNumber
+  )} - false`
+);
+
+array = [23, "abc", 22];
+console.log(
+  `using "some" function for even numbers array: ${array}, function ${evenNumber}, result: ${some(
+    array,
+    evenNumber
+  )} - true`
+);
+
+console.log(
+  `using "every" function for even numbers array: ${array}, function ${evenNumber}, result: ${every(
+    array,
+    evenNumber
+  )} - false`
+);
+
+array = [54, 58, 22];
+console.log(
+  `using "every" function for even numbers array: ${array}, function ${evenNumber}, result: ${every(
+    array,
+    evenNumber
+  )} - true`
+);
+
+array = [2, 3, 4];
+console.log(
+  `using "every" function for elements greater than the index values, array: ${array}, function ${elmGreaterIndex}, result: ${every(
+    array,
+    elmGreaterIndex
+  )} - true`
+);
+
+array = [-1, 0, 0];
+console.log(
+  `using "every" function for elements greater than the index values, array: ${array}, function ${elmGreaterIndex}, result: ${every(
+    array,
+    elmGreaterIndex
+  )} - false`
+);
